d54d2f9e8824ebbcb4c1a05475aa97cab712a895
Born24 <106234496+Born24@users.noreply.github.com>
2025-02-12 15:22:58 +0300

    Bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,28 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+
+        System.out.println(bean==bean2);
+        System.out.println(cat==cat2);
+
+        System.out.println(bean);
+        System.out.println(bean2);
+        System.out.println();
+        System.out.println(cat);
+        System.out.println(cat2);
     }
 }
